b1575db315b9d1f1da9d35a1166924afa9b52ef9
Bey Sviatoslav Igorevich <bey.sviatoslav@yandex.ru>
2020-07-23 02:46:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/bey_sviatoslav/android/dogbreedsapplication/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.bey_sviatoslav.android.dogbreedsapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.bey_sviatoslav.android.dogbreedsapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
